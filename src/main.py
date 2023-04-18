3e16840b91116ab91e1afc892ef83720c9ff5ea8
jtlong82 <JTLONG@FIRSTENERGYCORP.COM>
2023-04-18 07:58:22 -0400

    test

---
+++ src/main.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import filedialog
+from excel_to_dataframe import excel_to_dataframes
+
+def main():
+    # Create a tkinter root window (it won't be shown)
+    root = tk.Tk()
+    root.withdraw()
+
+    # Prompt the user to select a file using a file dialog
+    file_path = filedialog.askopenfilename(
+        title='Select an Excel file',
+        filetypes=(('Excel files', '*.xlsx'), ('All files', '*.*'))
+    )
+
+    # If the user selects a file, read the sheets and print the DataFrames
+    if file_path:
+        dataframes = excel_to_dataframes(file_path)
+
+        for sheet_name, df in dataframes.items():
+            print(f'Sheet name: {sheet_name}')
+            print(df)
+            print('\n')
+    else:
+        print('No file was selected.')
+
+if __name__ == '__main__':
+    main()
