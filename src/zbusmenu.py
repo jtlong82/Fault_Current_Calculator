ba1dc51874477bac8d9abe3c62ea5c7ae313d19f
jtlong82 <JTLONG@FIRSTENERGYCORP.COM>
2023-04-21 13:52:18 -0400

    changes

--- src/zbusmenu.py
+++ src/zbusmenu.py
@@ -7,23 +7,20 @@ def zbusmenu(dataframes):
     voltage_choice = int(input("Enter the number corresponding to the voltage level: ")) - 1
     selected_voltage = voltage_levels[voltage_choice]
 
-    # Get the DataFrame corresponding to the selected voltage level
+    # Select a record from the chosen voltage level DataFrame
     voltage_df = dataframes[selected_voltage]
+    voltage_df = voltage_df.reset_index(drop=True)  # Reset index to avoid any issues
+    stations = voltage_df.iloc[1:, 0].tolist()  # Get station names from the first column, excluding the header row
 
-    print("\nPlease select a station:")
-    station_names = voltage_df.iloc[1:, 0].tolist()  # Assuming station names are in the first column, starting from row 1
-    for idx, station in enumerate(station_names):
-        print(f"{idx + 1}. {station}")
+    # Print menu for station selection
+    print("Please select a station:")
+    for idx, station in enumerate(stations, start=1):
+        print(f"{idx}. {station}")
 
-    station_choice = int(input("Enter the number corresponding to the station: ")) - 1
-    selected_station = station_names[station_choice]
+    # Get user input for station selection
+    selected_station = int(input("Enter the number corresponding to the station you want to select: ")) - 1
 
-    # Get the selected record from the DataFrame
-    selected_record = voltage_df.iloc[station_choice + 1]  # Add 1 to account for the excluded label row
-
-    # Print the selected record with labels
-    print("\nSelected Record:")
-    for label, value in zip(voltage_df.iloc[0], selected_record):
-        print(f"{label}: {value}")
+    selected_record = voltage_df.iloc[selected_station + 1]  # Add 1 to account for the excluded label row
+    selected_record.index = voltage_df.iloc[0]  # Set the index of the selected_record to the column names from the original DataFrame
 
     return selected_record
